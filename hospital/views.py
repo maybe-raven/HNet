f4818b1b89f8ae5e0db975a689ea656b65eb6242
Imris <imris.curry@grimoire.us>
2017-04-14 14:27:01 -0400

    Revisions to Discharge Patient
    Make sure superusers cannot discharge patient, and make sure patients
    cannot be discharged multiple times.

--- hospital/views.py
+++ hospital/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from django.shortcuts import render, get_object_or_404
 from hospital.models import TreatmentSession
 from hnet.logger import CreateLogEntry
@@ -7,8 +7,12 @@ from hnet.logger import CreateLogEntry
 
 @login_required
 @permission_required('hospital.change_treatmentsession')
+@user_passes_test(lambda u: not u.is_superuser)
 def discharge_patient(request, treatmentsession_id):
     session = get_object_or_404(TreatmentSession, pk=treatmentsession_id)
+
+    if session.discharge_timestamp is not None:
+        return render(request, 'discharge/already_discharged.html')
 
     if request.method == 'POST':
         session.discharge_timestamp = datetime.now()
