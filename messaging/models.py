cc5e1fed85bc572195167c00b2eb0639212a09c8
Shiyu Du <dsy1998@live.com>
2017-04-21 18:47:53 -0400

    Implement the "send message" feature.

--- messaging/models.py
+++ messaging/models.py
@@ -9,3 +9,6 @@ class Message(models.Model):
     content = models.TextField()
 
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
--- messaging/views.py
+++ messaging/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import MessageForm
 
 
+@login_required
 def send_message(request):
-    # Not implemented
-    pass
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        form.sender = request.user
+        if form.is_valid():
+            form.save()
+            return render(request, 'messaging/send_done.html')
+    else:
+        form = MessageForm()
+
+    return render(request, 'messaging/send.html', {'form': form})
