d64bd0adfd947bfe65874ed8374048c5e9ee0af0
Abdulrahman Alfahad <aa8356@rit.edu>
2017-05-01 15:09:22 -0400

    Medical tests, incomplete
    Incomplete test

--- medical/tests.py
+++ medical/tests.py
@@ -7,7 +7,7 @@ from medical.views import upload_test_result
 
 
 # Create your tests here.
-"""
+
 
 class uploadfiletest(TestCase):
     def setup(self):
@@ -20,6 +20,7 @@ class uploadfiletest(TestCase):
         doctor = User.objects.get(username=username).doctor
         patient = User.objects.get(username=patientu).patient
         diagnosis = Diagnosis.objects.create(patient=patient, summery=" ", catagory=" ")
-        test =
-        request = self.factory.post(reverse('medical:upload_test_result', args=[doctor.id]))
-"""
+        test = Test.objects.create(doctot=doctor, diagnosis=diagnosis, description="", results="", notes="")
+        request = self.factory.post(reverse('medical:upload_test_result', args=[test.id]))
+        response = self.client.post(reverse('account:register_patient'))
+        self.assertEqual(response.status_code, 302, 'Expected to be redirected due to insufficient permission.')
